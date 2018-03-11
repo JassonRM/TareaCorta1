e397319942d2bf4d001bdc72f319189ba00c2d8b
Unknown <jassonrm@icloud.com>
2018-03-11 10:47:23 -0600

    Proyecto CLion

---
+++ bigarray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jasson Rodríguez Méndez on 11/3/18.
+//
+
+#include "bigarray.h"
---
+++ bigarray.h
@@ -0,0 +1,15 @@
+//
+// Created by Jasson Rodríguez Méndez on 11/3/18.
+//
+
+#ifndef TAREACORTA1_BIGARRAY_H
+#define TAREACORTA1_BIGARRAY_H
+
+
+class bigarray {
+    int pagesize = 100;
+    
+};
+
+
+#endif //TAREACORTA1_BIGARRAY_H
