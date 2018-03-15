8abe926d896cd3772e18f32671c31e1f017b03fc
Unknown <jassonrm@icloud.com>
2018-03-15 01:47:47 -0600

    Auto stash before merge of "master" and "origin/master"
    Operator[] Error con la const buffer_size

--- bigarray.cpp
+++ bigarray.cpp
@@ -3,12 +3,8 @@
 //
 
 #include "bigarray.h"
-/**
- * Escribe una página específica a disco.
- * @param beg valor del byte a partir del cual escribir
- * @param page página que se desea escirbir
- */
-void bigarray::savePage(int beg,page page1) {
+
+void bigarray::savePage(int beg,page *page1) {
 
     std::string path = "../IntGenerator/aBigArray.bin";
     std::ofstream file(path, std::ios::binary);
@@ -21,7 +17,7 @@ void bigarray::savePage(int beg,page page1) {
 
         for (int i = 0; i < this->pagesize; i++){
 
-            file.write(reinterpret_cast<char *>(&(page1.data)[i]), bytes);
+            file.write(reinterpret_cast<char *>(&(page1->data)[i]), bytes);
 
         }
         file.close();
@@ -31,12 +27,8 @@ void bigarray::savePage(int beg,page page1) {
     }
 
 }
-/**
- * Lee una página específica a disco.
- * @param beg valor del byte a partir del cual leer
- * @param page página donde se cargarán los datos
- */
-void bigarray::loadPage(int beg, page page1){
+
+void bigarray::loadPage(int beg, page *page1){
 
     std::string path = "../IntGenerator/aBigArray.bin";
     std::ifstream file(path, std::ios::binary);
@@ -48,7 +40,7 @@ void bigarray::loadPage(int beg, page page1){
 
         for (int i = 0; i < this->pagesize; i++){
 
-            file.read(reinterpret_cast<char *>(&(page1.data)[i]), bytes);
+            file.read(reinterpret_cast<char *>(&(page1->data)[i]), bytes);
 
         }
         file.close();
@@ -57,3 +49,35 @@ void bigarray::loadPage(int beg, page page1){
         std::cout<<"Error al abrir el archivo"<<std::endl;
     }
 }
+
+int& bigarray::operator[](int index) {
+    int current = index / pagesize;
+    for(int i = 0; i < buffer_size; i++){
+        if(loaded[i]->pageNum == current){
+            page *used = loaded[0];
+            loaded[0] = loaded[i];
+            loaded[i] = used;
+            return used->data[index - current * pagesize];
+        }else{
+            loaded[buffer_size - 1]->pageNum = current;
+            loadPage(current * pagesize, loaded[buffer_size - 1]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bigarray.h
+++ bigarray.h
@@ -13,13 +13,45 @@
 
 class bigarray {
 private:
+    /**
+     * Number of Int per page
+     */
     int pagesize = 100;
     page page1;
     page page2;
     page page3;
 
-    void savePage(int beg, page page1);
-    void loadPage(int beg, page page1);
+    /**
+     * Number of pages in memory
+     */
+    int buffer_size = 3;
+
+    /**
+     * Pages loaded in memory
+     */
+    page *loaded[buffer_size];
+
+    /**
+     * Subscripting bigarray
+     * @param index
+     * @return value located at index
+     */
+    int &operator[](int index){
+        int current = index / pagesize;
+        for(int i = 0; i < buffer_size, i++){
+            if(loaded[i] == current){
+                page used = loaded[0];
+                loaded[0] = loaded[i];
+                loaded[i] = used;
+                return used.data[index - current * pagesize];
+            }else{
+                delete loaded[buffer_size - 1];
+                page *load = new page(current);
+                load.data =
+                loaded[buffer_size - 1];
+            }
+        }
+    }
 
 };
 
