14f9641946eecb1ba4a7a434bbef605ecc332be2
Unknown <jassonrm@icloud.com>
2018-03-15 01:48:31 -0600

    Arreglo header

--- bigarray.h
+++ bigarray.h
@@ -17,41 +17,38 @@ private:
      * Number of Int per page
      */
     int pagesize = 100;
-    page page1;
-    page page2;
-    page page3;
 
     /**
      * Number of pages in memory
      */
-    int buffer_size = 3;
+    const int buffer_size = 3;
 
     /**
      * Pages loaded in memory
      */
     page *loaded[buffer_size];
 
+public:
+    /**
+     * Escribe una página específica a disco.
+     * @param beg valor del byte a partir del cual escribir
+     * @param page página que se desea escirbir
+     */
+    void savePage(int beg,page *page1);
+
+    /**
+     * Lee una página específica a disco.
+     * @param beg valor del byte a partir del cual leer
+     * @param page página donde se cargarán los datos
+     */
+    void loadPage(int beg, page *page1);
+
     /**
      * Subscripting bigarray
      * @param index
      * @return value located at index
      */
-    int &operator[](int index){
-        int current = index / pagesize;
-        for(int i = 0; i < buffer_size, i++){
-            if(loaded[i] == current){
-                page used = loaded[0];
-                loaded[0] = loaded[i];
-                loaded[i] = used;
-                return used.data[index - current * pagesize];
-            }else{
-                delete loaded[buffer_size - 1];
-                page *load = new page(current);
-                load.data =
-                loaded[buffer_size - 1];
-            }
-        }
-    }
+    int &operator[](int index);
 
 };
 
